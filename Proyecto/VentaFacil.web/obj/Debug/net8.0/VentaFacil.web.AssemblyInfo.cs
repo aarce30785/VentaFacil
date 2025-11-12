5b41a3dff90a490429a87ffd73eb826ca8eb6e24
antonyarce <antonycr20@hotmail.com>
2025-11-12 06:23:17 -0600

    Confirmar cambios

--- Proyecto/VentaFacil.web/obj/Debug/net8.0/VentaFacil.web.AssemblyInfo.cs
+++ Proyecto/VentaFacil.web/obj/Debug/net8.0/VentaFacil.web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VentaFacil.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c352de16e67c2caba06eed6778a8d42bbcc3ef2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900ada24a0349aa785b5d7e4eedf38d85955b6db")]
 [assembly: System.Reflection.AssemblyProductAttribute("VentaFacil.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VentaFacil.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
